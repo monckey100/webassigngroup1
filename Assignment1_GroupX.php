1853135a2751b5c3be9ef39b4453299448055584
Rafael Henrique Machado <rafael_henriquemachado@hotmail.com>
2019-03-04 19:12:48 -0800

    Page added

---
+++ Assignment1_GroupX.php
@@ -0,0 +1,14 @@
+<?php
+
+require("inc/config.inc.php");
+require("inc/Book.class.php");
+require("inc/FileAgent.class.php");
+require("inc/Page.class.php");
+require("inc/Person.class.php");
+require("inc/Validation.class.php");
+
+Page::$title = "Assignment 1 - Group X";
+Page::header();
+Page::footer();
+
+?>
